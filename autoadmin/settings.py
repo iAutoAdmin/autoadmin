22c53a509897cd443dca8f27362462860114f6e3
rongjunfeng@cmcm.com <rongjunfeng@cmcm.com>
2019-08-03 11:26:14 +0800

    增加rest

--- autoadmin/settings.py
+++ autoadmin/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'django_filters',
     'users'
 ]
 
--- autoadmin/urls.py
+++ autoadmin/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework.routers import DefaultRouter
+from rest_framework_jwt.views import obtain_jwt_token
+from users.router import user_router
+
+router = DefaultRouter()
+router.registry.extend(user_router.registry)
+
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^docs/', include_docs_urls("autoadmin"))
 ]
