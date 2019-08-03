0dc5210c58d8127e0ed561759cab3965d65b1639
rongjunfeng@cmcm.com <rongjunfeng@cmcm.com>
2019-08-03 14:18:13 +0800

    add

--- apps/users/router.py
+++ apps/users/router.py
@@ -3,6 +3,6 @@ from .views import UserRegViewset, UsersViewset, UserInfoViewset
 
 
 user_router = DefaultRouter()
-user_router.register(r'userReg', UserRegViewset, base_name="userReg")
+user_router.register(r'user/Reg', UserRegViewset, base_name="userReg")
 user_router.register(r'users', UsersViewset, base_name="users")
 user_router.register(r'userInfo', UserInfoViewset, base_name="userInfo")
