b6015ac61eb16613922452fa96b32c09f36508e9
mt <mt@qq.com>
2019-08-06 20:46:25 +0800

    获取ecs

--- apps/clouds/aliyun/common.py
+++ apps/clouds/aliyun/common.py
@@ -7,6 +7,8 @@ from aliyunsdkcore.acs_exception.exceptions import ClientException
 from aliyunsdkcore.acs_exception.exceptions import ServerException
 from aliyunsdkslb.request.v20140515.DescribeRegionsRequest import DescribeRegionsRequest
 from aliyunsdkslb.request.v20140515.DescribeLoadBalancersRequest import DescribeLoadBalancersRequest
+from aliyunsdkslb.request.v20140515.DescribeLoadBalancerAttributeRequest import DescribeLoadBalancerAttributeRequest
+from aliyunsdkecs.request.v20140526.DescribeInstancesRequest import DescribeInstancesRequest
 
 
 class ALiYun(object):
@@ -35,22 +37,56 @@ class ALiYun(object):
         :return:
         """
         balancers = []
-        regions = self.get_regions()
+        regionids = self.get_regions()
         try:
-            for region in regions:
-                client = AcsClient(self.accessKeyId, self.accessSecret, region, connect_timeout=30)
+            for rid in regionids:
+                client = AcsClient(self.accessKeyId, self.accessSecret, rid, connect_timeout=30)
                 request = DescribeLoadBalancersRequest()
                 request.set_accept_format('json')
                 response = client.do_action_with_exception(request)
                 res = eval(str(response, encoding='utf-8'))
-                # print(res.get("LoadBalancers").get("LoadBalancer"))
                 balancers += res.get("LoadBalancers").get("LoadBalancer")
             return balancers
         except Exception as ex:
             print(ex)
 
+    def get_slb_detail(self, loadbalancerid, regionid):
+        """
+        返回lb详细信息
+        :param loadbalancerid: lb-2zeqx4f9qglel963dmdzv
+        :param regionid: cn-beijing
+        :return:
+        """
+        client = AcsClient(self.accessKeyId, self.accessSecret, regionid)
+        request = DescribeLoadBalancerAttributeRequest()
+        request.set_accept_format('json')
+        request.set_LoadBalancerId(loadbalancerid)
+        response = client.do_action_with_exception(request)
+        return eval(str(response, encoding='utf-8'))
+
+    def get_ecs(self):
+        """
+        获取所有区域ECS信息
+        :return:
+        """
+        instances = []
+        regionids = self.get_regions()
+        try:
+            for rid in regionids:
+                client = AcsClient(self.accessKeyId, self.accessSecret, rid, connect_timeout=30)
+                request = DescribeInstancesRequest()
+                request.set_accept_format('json')
+                response = client.do_action_with_exception(request)
+                res = json.loads(str(response, encoding='utf-8'))
+                instances += res['Instances']['Instance']
+            return instances
+        except Exception as ex:
+            return ex
+
 
 if __name__ == '__main__':
     ali = ALiYun()
     # ali.get_regions()
-    print(ali.get_slb())
+    # print(ali.get_slb())
+    # print(ali.get_slb_detail("lb-2zeqx4f9qglel963dmdzv", "cn-beijing"))
+    print(ali.get_ecs())
