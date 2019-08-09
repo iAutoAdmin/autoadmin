00734915469748880104666227c54832ed674ac2
mt <mt@qq.com>
2019-08-09 21:10:55 +0800

    完善ECS主机接口

--- apps/clouds/aliyun/common.py
+++ apps/clouds/aliyun/common.py
@@ -6,31 +6,20 @@ from aliyunsdkcore.client import AcsClient
 from aliyunsdkcore.acs_exception.exceptions import ClientException
 from aliyunsdkcore.acs_exception.exceptions import ServerException
 from aliyunsdkecs.request.v20140526.DescribeZonesRequest import DescribeZonesRequest
-from aliyunsdkecs.request.v20140526.DescribeAvailableResourceRequest import DescribeAvailableResourceRequest
 from aliyunsdkslb.request.v20140515.DescribeRegionsRequest import DescribeRegionsRequest
 from aliyunsdkslb.request.v20140515.DescribeLoadBalancersRequest import DescribeLoadBalancersRequest
 from aliyunsdkslb.request.v20140515.DescribeLoadBalancerAttributeRequest import DescribeLoadBalancerAttributeRequest
 from aliyunsdkecs.request.v20140526.DescribeInstancesRequest import DescribeInstancesRequest
 from aliyunsdkecs.request.v20140526.DescribeInstanceTypesRequest import DescribeInstanceTypesRequest
-from aliyunsdkecs.request.v20140526.DescribeInstanceTypeFamiliesRequest import DescribeInstanceTypeFamiliesRequest
+from aliyunsdkecs.request.v20140526.StopInstanceRequest import StopInstanceRequest
+from aliyunsdkecs.request.v20140526.StartInstanceRequest import StartInstanceRequest
+from aliyunsdkecs.request.v20140526.DescribeDisksRequest import DescribeDisksRequest
+
 
 class ALiYun(object):
     def __init__(self):
         self.accessKeyId = accessKeyId
         self.accessSecret = accessSecret
-
-    def get_zone(self, RegionId):
-        """
-        查询RegionId下的可用区，根据ZoneId判断LocalName
-        :param RegionId:
-        :return:
-        """
-        client = AcsClient(self.accessKeyId, self.accessSecret, RegionId)
-        request = DescribeZonesRequest()
-        request.set_accept_format('json')
-        response = client.do_action_with_exception(request)
-        res = str(response, encoding='utf-8')
-        return json.loads(res).get("Zones").get("Zone")
 
     def get_regions(self):
         """
@@ -46,6 +35,19 @@ class ALiYun(object):
         for region in (eval(res)['Regions']['Region']):
             regions.append(region['RegionId'])
         return regions
+
+    def get_zone(self, RegionId):
+        """
+        查询RegionId下的可用区，根据ZoneId判断LocalName
+        :param RegionId:
+        :return:
+        """
+        client = AcsClient(self.accessKeyId, self.accessSecret, RegionId)
+        request = DescribeZonesRequest()
+        request.set_accept_format('json')
+        response = client.do_action_with_exception(request)
+        res = str(response, encoding='utf-8')
+        return json.loads(res).get("Zones").get("Zone")
 
     def get_slb(self):
         """
@@ -93,7 +95,6 @@ class ALiYun(object):
                 request = DescribeInstancesRequest()
                 request.set_accept_format('json')
                 response = client.do_action_with_exception(request)
-                # print(str(response, encoding='utf-8'))
                 res = json.loads(str(response, encoding='utf-8'))
                 instances += res['Instances']['Instance']
             return instances
@@ -114,6 +115,48 @@ class ALiYun(object):
         res = str(response, encoding='utf-8')
         return json.loads(res).get("InstanceTypes").get("InstanceType")
 
+    def stop_instance(self, instanceid):
+        """
+        停止实例
+        :param instanceid:
+        :return:
+        """
+        client = AcsClient(self.accessKeyId, self.accessSecret, instanceid)
+        request = StopInstanceRequest()
+        request.set_accept_format('json')
+        response = client.do_action_with_exception(request)
+        print(str(response, encoding='utf-8'))
+
+    def start_instance(self, instanceid):
+        """
+        启动实例
+        :param instanceid:
+        :return:
+        """
+        client = AcsClient(self.accessKeyId, self.accessSecret, instanceid)
+        request = StartInstanceRequest()
+        request.set_accept_format('json')
+        response = client.do_action_with_exception(request)
+        print(str(response, encoding='utf-8'))
+
+    def get_instance_disk_info(self, regionid, zoneid, instanceid):
+        """
+        获取实例下磁盘信息
+        :param regionid: cn-beijing
+        :param zoneid: cn-beijing-g
+        :param instanceid: i-2ze4wxcyl324952q8m7b
+        :return:
+        """
+        client = AcsClient(self.accessKeyId, self.accessSecret, regionid)
+        request = DescribeDisksRequest()
+        request.set_accept_format('json')
+        request.set_ZoneId(zoneid)
+        request.set_InstanceId(instanceid)
+        response = client.do_action_with_exception(request)
+        # print((str(response, encoding='utf-8')))
+        res = json.loads((str(response, encoding='utf-8')))
+        return res['Disks']['Disk']
+
 if __name__ == '__main__':
     ali = ALiYun()
     # ali.get_regions()
@@ -125,3 +168,4 @@ if __name__ == '__main__':
     # res = ali.get_instancetype("cn-beijing")
     # for i in res:
     #     print(i["CpuCoreCount"],i["MemorySize"])
+    print(ali.get_instance_disk_info("cn-beijing", "cn-beijing-g", "i-2ze4wxcyl324952q8m7b"))
--- apps/clouds/aliyun/task.py
+++ apps/clouds/aliyun/task.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 import hashlib
+import datetime
 from clouds.aliyun.common import ALiYun
 
 
@@ -12,27 +13,41 @@ def md5(str):
 
 
 def rsync_instances():
+    starttime = datetime.datetime.now()
     ali = ALiYun()
     instances = ali.get_ecs()
     for instance in instances:
-        print(md5(instance['InstanceId']))
-        print(instance['InstanceName'])
-        regionid = instance.get("ZoneId").split("-")[0]+"-" + instance.get("ZoneId").split("-")[1]
-        zones = ali.get_zone(regionid)
-        for i in zones:
-            if instance.get("ZoneId") == i["ZoneId"]:
-                print(i["LocalName"])
-        print(instance['VpcAttributes']['PrivateIpAddress']['IpAddress'][0])
-        print(instance.get('PublicIpAddress').get('IpAddress')[0])
-        print(instance['InstanceNetworkType'])
-        print(instance['InstanceType'])
-        print(instance['Cpu'])
-        print(instance['Memory']/1024)
-        print(instance['Status'])
-        print(instance['ExpiredTime'])
-        print(str(instance['InternetMaxBandwidthOut']))
-
-
+        print(instance.get('InstanceId'))
+        print(md5(instance.get('InstanceId')))
+        print(instance.get('InstanceName'))
+        print(instance.get('HostName'))
+        RegionId = instance.get('RegionId')
+        print(RegionId)
+        print(instance.get("ZoneId"))
+        zones = ali.get_zone(RegionId)
+        for zone in zones:
+            if instance.get("ZoneId") == zone["ZoneId"]:
+                print(zone["LocalName"])
+        disks = ali.get_instance_disk_info(RegionId, instance.get("ZoneId"), instance.get('InstanceId'))
+        for disk in disks:
+            print(disk.get('Size'))
+            print(disk.get('Type'))
+        print(instance['OSName'])
+        print(instance.get('VpcAttributes').get('PrivateIpAddress').get('IpAddress')[0])
+        print(instance.get('PublicIpAddress').get('IpAddress')[0], "")
+        print(instance.get('Status'))
+        print(instance.get('InstanceNetworkType'))
+        print(instance.get('Cpu'))
+        print(instance.get('Memory') / 1024)
+        print(instance.get('IoOptimized'))
+        print(instance.get('GPUAmount'))
+        print(instance.get('InternetMaxBandwidthOut'))
+        print(instance.get('InstanceType'))
+        print(instance.get('InstanceChargeType'))
+        print(instance.get('CreationTime'))
+        print(instance.get('ExpiredTime'))
+    endtime = datetime.datetime.now()
+    print(endtime - starttime)
 
 
 if __name__ == '__main__':
